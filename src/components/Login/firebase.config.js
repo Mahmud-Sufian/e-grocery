63ac1cb8bbd6f8240db1432adb6364d1fba60da4
Mahmud-Sufian <mahmud5281585@gmail.com>
2021-03-31 11:11:45 -0700

    done

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBLDKJT-eWmCDYofUSEpXbfkocSwaEaN6Y",
-    authDomain: "e-grocery-dcb43.firebaseapp.com",
-    projectId: "e-grocery-dcb43",
-    storageBucket: "e-grocery-dcb43.appspot.com",
-    messagingSenderId: "964324915635",
-    appId: "1:964324915635:web:a271ca4f0335e301f1f3c6"
-  };
+  apiKey: "AIzaSyBTeDMg-XgHuv9sdYAomOliSbyQDOD2ooA",
+  authDomain: "grocery-c8cc1.firebaseapp.com",
+  projectId: "grocery-c8cc1",
+  storageBucket: "grocery-c8cc1.appspot.com",
+  messagingSenderId: "1035202249570",
+  appId: "1:1035202249570:web:1f287c6d192e32e818e23b"
+};
   export default firebaseConfig;
